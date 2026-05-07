a76a522610da34e57532c1d1b5d371f222c94d5e
Cardoszo7 <1251212@isep.ipp.pt>
2026-05-07 20:56:20 +0100

    update cargacontainer

--- Project/Headers/Model/CargaContainer.h
+++ Project/Headers/Model/CargaContainer.h
@@ -1,30 +1,20 @@
 #ifndef CARGACONTAINER_H
 #define CARGACONTAINER_H
 
-#include <list>
+#include <string>
 #include <vector>
 #include "Carga.h"
 
-
 class CargaContainer {
 private:
-    std::list<Carga> cargas; ///< Lista interna de todas as cargas registadas
+    std::vector<Carga> cargas;
 
 public:
-   
-    CargaContainer() = default;
-
     void guardar(Carga carga);
-
-    std::list<Carga>& getTodos();
-
-
+    std::vector<Carga>& getTodos();
     Carga* procurar(int indice);
-
-    
     void remover(int indice);
-
     std::vector<Carga*> getDisponiveis();
 };
 
-#endif // CARGACONTAINER_H
+#endif
