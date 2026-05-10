3d9923b6ab9741ddff552dd515de919175f15544
1250717 <1250717@isep.ipp.pt>
2026-05-10 18:37:24 +0100

    Update CamionistaContainer.cpp

--- Project/Src/Model/CamionistaContainer.cpp
+++ Project/Src/Model/CamionistaContainer.cpp
@@ -1,14 +1,41 @@
 #include "Project\Headers\Model\CamionistaContainer.h"
 
-void CamionistaContainer::guardar(Camionista camionista){
+Camionista* CamionistaContainer::validarNome(std::string nome) {
+    return procurar(nome);
+}
+
+void CamionistaContainer::guardar(Camionista camionista) {
     camionistas.push_back(camionista);
 }
 
-Camionista* CamionistaContainer::procurar(std::string nome){
-    for(int i=0; i<camionistas.size(); i++){
-        if(camionistas[i].getNome() == nome){
+std::vector<Camionista>& CamionistaContainer::getTodos() {
+    return camionistas;
+}
+
+std::vector<Camionista*> CamionistaContainer::getDisponiveis() {
+    std::vector<Camionista*> disponiveis;
+    for (int i = 0; i < (int)camionistas.size(); i++) {
+        if (camionistas[i].getEstado() == "Disponível") {
+            disponiveis.push_back(&camionistas[i]);
+        }
+    }
+    return disponiveis;
+}
+
+Camionista* CamionistaContainer::procurar(std::string nome) {
+    for (int i = 0; i < (int)camionistas.size(); i++) {
+        if (camionistas[i].getNome() == nome) {
             return &camionistas[i];
         }
     }
     return nullptr;
+}
+
+void CamionistaContainer::remover(std::string nome) {
+    for (int i = 0; i < (int)camionistas.size(); i++) {
+        if (camionistas[i].getNome() == nome) {
+            camionistas.erase(camionistas.begin() + i);
+            return;
+        }
+    }
 }
