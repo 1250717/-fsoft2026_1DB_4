472b177da9bcf3b855eae08a5af263cf88205cf2
Cardoszo7 <1251212@isep.ipp.pt>
2026-05-20 22:44:31 +0100

    CargaMapper.h, RotaMapper.h
    Nao completo

---
+++ Project/Headers/Mapper/CargaMapper.h
@@ -0,0 +1,7 @@
+#ifndef CARGAMAPPER_H
+#define CARGAMAPPER_H
+
+#include "../Model/Carga.h"
+#include "../Dtos/CargaDTO.h"
+
+#endif
---
+++ Project/Headers/Mapper/RotaMapper.h
@@ -0,0 +1,7 @@
+#ifndef ROTAMAPPER_H
+#define ROTAMAPPER_H
+
+#include "../Model/Rota.h"
+#include "../Dtos/RotaDTO.h"
+
+#endif
