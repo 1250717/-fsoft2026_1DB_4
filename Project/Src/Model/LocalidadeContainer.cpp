45e83fadd3b36932dfc289b01e69cf7f2fb65af7
vilasboas72 <1250970@isep.ipp.pt>
2026-05-25 17:01:08 +0100

    Update LocalidadeContainer.cpp

---
+++ Project/Src/Model/LocalidadeContainer.cpp
@@ -0,0 +1,27 @@
+#include "..\..\Headers\Model\LocalidadeContainer.h"
+
+void LocalidadeContainer::guardar(Localidade localidade) {
+    localidades.push_back(localidade);
+}
+
+std::vector<Localidade>& LocalidadeContainer::getTodos() {
+    return localidades;
+}
+
+Localidade* LocalidadeContainer::procurar(std::string nome) {
+    for (int i = 0; i < (int)localidades.size(); i++) {
+        if (localidades[i].getNome() == nome) {
+            return &localidades[i];
+        }
+    }
+    return nullptr;
+}
+
+void LocalidadeContainer::remover(std::string nome) {
+    for (int i = 0; i < (int)localidades.size(); i++) {
+        if (localidades[i].getNome() == nome) {
+            localidades.erase(localidades.begin() + i);
+            return;
+        }
+    }
+}
