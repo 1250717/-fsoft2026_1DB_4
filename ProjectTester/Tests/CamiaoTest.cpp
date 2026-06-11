42bd51cb22ef2e39a3fc0d07bd623386a0929d3a
BSe21 <1250795@isep.ipp.pt>
2026-06-11 12:10:41 +0100

    Update Test

--- ProjectTester/Tests/CamiaoTest.cpp
+++ ProjectTester/Tests/CamiaoTest.cpp
@@ -1,4 +1,4 @@
-#include <gtest/gtest.h>
+#include "../../ProjectTester/googletest/include/gtest/gtest.h"
 #include "../../Project/Headers/Model/Camiao.h"
 #include "../../Project/Headers/Model/Camionista.h"
 #include "../../Project/Headers/Model/Carga.h"
@@ -77,4 +77,22 @@ TEST(CamiaoGestaoCargas, RemoverCargaDecrementaLista) {
     camiao.removerCarga(&carga);
     //Assert
     EXPECT_EQ(camiao.getCargas().size(), 0u);
+}
+
+TEST(CamiaoSettersTest, SetEstadoAtualizado) {
+    //Arrange
+    Camiao camiao("AB-12-CD", 500.0f);
+    //Act
+    camiao.setEstado("Atribuido");
+    //Assert
+    EXPECT_EQ(camiao.getEstado(), "Atribuido");
+}
+ 
+TEST(CamiaoSettersTest, SetCapacidadeDisponivelAtualizado) {
+    //Arrange
+    Camiao camiao("AB-12-CD", 500.0f);
+    //Act
+    camiao.setCapacidadeDisponivel(300.0f);
+    //Assert
+    EXPECT_FLOAT_EQ(camiao.getCapacidadeDisponivel(), 300.0f);
 }
