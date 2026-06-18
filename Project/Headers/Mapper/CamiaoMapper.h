7b5d2e3e2d67cf5fee538f701e962d108d881dce
1250717 <1250717@isep.ipp.pt>
2026-06-18 09:49:49 +0100

    Descobrimos alguns problemas antes da apresentacao

--- Project/Headers/Mapper/CamiaoMapper.h
+++ Project/Headers/Mapper/CamiaoMapper.h
@@ -6,6 +6,7 @@
 
 class CamiaoMapper {
 public:
+//nao é preciso criar um camiao Mapper para poder usa lo
     static CamiaoDTO toDTO(Camiao &camiao) {
         CamiaoDTO dto;
         dto.matricula = camiao.getMatricula();
